384995d461d31ad6ff1d88a500649b08f13b2da0
Iforfreee <igor.freee88@gmail.com>
2018-12-03 21:13:51 +0200

    2th

---
+++ Array.php
@@ -0,0 +1,11 @@
+<?php
+$a=['пн','вт','ср','чт','пт','сб','вс'];
+$a[]='ll';
+var_dump($a);
+echo '<br>';
+var_export($a);
+echo '<br>';
+echo $a[2];
+$a[]='2';
+echo <br>;
+
---
+++ Index.php
@@ -0,0 +1,13 @@
+<?php
+$a=1;
+$b=1;
+
+echo '<br>';
+
+
+echo date('d.m.y h.i:s');
+
+echo $a;
+
+
+
